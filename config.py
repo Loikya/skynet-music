9754fbad66d0ba3657fea95fdf787588ac0adf61
Владимир Лоик <vladimirloik@mail.ru>
2017-02-08 00:46:46 +0300

    First  commit

---
+++ config.py
@@ -0,0 +1,10 @@
+#lastfm api
+API_KEY = "478eda30f4199a51067369517f8be753"
+API_SECRET = "af2efcd726f10f93c1cab4355768d5ea"
+lfm_username = "RussianSkynet"
+lfm_password = "246133 vova"
+
+#vk api
+v_login= "89851906212"
+v_password = "228dicks228"
+app_id = "3697615"
